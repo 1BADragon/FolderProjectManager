e264259ecd5c535157a7b49a02c7869ffbb62c49
1BADragon <6611786+1BADragon@users.noreply.github.com>
2022-02-22 21:03:12 -0800

    Working on settings

--- folderprojectsettings.cpp
+++ folderprojectsettings.cpp
@@ -1,6 +1,82 @@
 #include "folderprojectsettings.h"
 
-FolderProjectSettings::FolderProjectSettings()
-{
+namespace FolderProjectManager {
+namespace Internal {
 
+static FolderProjectSettings default_settings;
+
+ValueRef Value::object()
+{
+    auto r = std::make_shared<Value>();
+
+    r->_data = Object();
+    return r;
+}
+
+ValueRef Value::array()
+{
+    auto r = std::make_shared<Value>();
+
+    r->_data = Array();
+    return r;
+}
+
+ValueRef Value::integer(long long v)
+{
+    auto r = std::make_shared<Value>();
+
+    (*r) = v;
+    return r;
+}
+
+ValueRef Value::real(double d)
+{
+    auto r = std::make_shared<Value>();
+
+    (*r) = d;
+    return r;
+}
+
+ValueRef Value::string(const std::string &s)
+{
+    auto r = std::make_shared<Value>();
+
+    (*r) = s;
+    return r;
+}
+
+ValueRef Value::operator[](size_t index)
+{
+    return std::get<Array>(_data)[index];
+}
+
+ValueRef Value::operator[](const std::string &key)
+{
+    return std::get<Object>(_data)[key];
+}
+
+Value& Value::operator=(long long v)
+{
+    this->_data = v;
+    return *this;
+}
+
+Value& Value::operator=(double d)
+{
+    this->_data = d;
+    return *this;
+}
+
+Value& Value::operator=(const std::string &s)
+{
+    this->_data = s;
+    return *this;
+}
+
+FolderProjectSettings& getDefault()
+{
+    return default_settings;
+}
+
+}
 }
--- folderprojectsettings.h
+++ folderprojectsettings.h
@@ -1,11 +1,56 @@
 #ifndef FOLDERPROJECTSETTINGS_H
 #define FOLDERPROJECTSETTINGS_H
 
+#include <variant>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <memory>
+
+namespace FolderProjectManager {
+namespace Internal {
+
+class Value;
+using ValueRef = std::shared_ptr<Value>;
+using Object = std::unordered_map<std::string, ValueRef>;
+using Array = std::vector<ValueRef>;
+class Value {
+    static ValueRef object();
+    static ValueRef array();
+    static ValueRef integer(long long v = 0);
+    static ValueRef real(double d = 0.f);
+    static ValueRef string(const std::string &s = "");
+
+
+    template<typename T>
+    T& get() const {
+        return std::get<T>(_data);
+    }
+
+    ValueRef operator[](size_t index);
+    ValueRef operator[](const std::string &key);
+
+    Value& operator=(long long v);
+    Value& operator=(double d);
+    Value& operator=(const std::string &s);
+
+private:
+    std::variant<Object, Array, long long, double, std::string> _data;
+};
 
 class FolderProjectSettings
 {
 public:
     FolderProjectSettings();
+
+private:
+    Object _base;
 };
+
+FolderProjectSettings& getDefault();
+
+}
+}
+
 
 #endif // FOLDERPROJECTSETTINGS_H
